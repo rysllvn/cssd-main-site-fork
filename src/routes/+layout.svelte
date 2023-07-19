41dea300171eff4806e58de03eeabc0d9a6cd75a
Ryan Sullivan <mrsulliv@gmail.com>
2023-07-18 21:19:44 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	// import '@skeletonlabs/skeleton/themes/theme-seafoam.css';
+	import '$lib/styles/theme.postcss';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import 'iconify-icon';
+
+	import {
+		AppBar,
+		AppRail,
+		AppRailAnchor,
+		AppRailTile,
+		AppShell,
+		LightSwitch
+	} from '@skeletonlabs/skeleton';
+
+	import logo from '$lib/assets/cssd-logo.jpg';
+
+	let currentTile = 0;
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<AppBar class="shadow-lg">
+			<svelte:fragment slot="lead">
+				<iconify-icon icon="noto-v1:sailboat" class="text-4xl" />
+			</svelte:fragment>
+			Challenged Sailors San Diego
+			<svelte:fragment slot="trail">
+				<button type="button" class="btn variant-filled">Facebook</button>
+				<button type="button" class="btn variant-filled">Instagram</button>
+				<button type="button" class="btn variant-filled">Twitter</button>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Insert the list: -->
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="/about">Participate</a></li>
+				<li><a href="/about">Volunteer</a></li>
+				<li><a href="/about">Give</a></li>
+				<li><a href="/about">Contact</a></li>
+				<li><a href="/about">Resources</a></li>
+			</ul>
+		</nav>
+		<!-- --- -->
+	</svelte:fragment>
+
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import logo from '$lib/assets/cssd-logo.jpg';
+	import groupSail from '$lib/assets/all-boats.png';
+</script>
+
+<div class="container mx-auto p-8 space-y-8">
+	<div class="flex gap-1">
+		<div class="w-1/2">
+			<img src={logo} alt="Challenged Sailors San Diego Logo" class="aspect-" />
+		</div>
+		<section>
+			<h2 class="h2">Our Mission</h2>
+			<p>
+				Challenged Sailors San Diego provides therapeutic and recreational adaptive sailing
+				opportunities for people with disabilities to enhance their dignity, well- being, and
+				independence.
+			</p>
+		</section>
+
+		<section>
+			<h2 class="h2">News</h2>
+			<ul>
+				<li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+				<hr />
+				<li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+				<hr />
+				<li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+			</ul>
+		</section>
+	</div>
+	<section>
+		<img src={groupSail} alt="" class="mb-10" />
+
+		<a class="btn variant-filled-primary" href="https://github.com/">Donate</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Participate</a>
+		<a class="btn variant-filled-tertiary" href="https://kit.svelte.dev/">Volunteer</a>
+	</section>
+</div>
