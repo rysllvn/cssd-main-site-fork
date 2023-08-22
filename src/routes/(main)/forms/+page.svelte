8ab16aa71a8043595fefba358e2af78d06dfd513
Ryan Sullivan <mrsulliv@gmail.com>
2023-08-22 10:41:50 -0700

    add manuals and forms

--- src/routes/(main)/forms/+page.svelte
+++ src/routes/(main)/forms/+page.svelte
@@ -1,1 +1,26 @@
+<script lang="ts">
+	import form501C from '$lib/assets/documents/501(c)(3) IRS Determination Letter Oct 2015.pdf';
+	import form9902020 from '$lib/assets/documents/990ez 2020.pdf';
+	import form9902021 from '$lib/assets/documents/990ez 2021 CSSD.pdf';
+
+	const forms = [
+		{
+			href: form501C,
+			title: '501(c)(3) IRS Determination Letter Oct 2015'
+		},
+		{ href: form9902020, title: '990ez 2020' },
+		{ href: form9902021, title: '990ez 2021' }
+	];
+</script>
+
 <h1 class="h1">Forms</h1>
+<hr />
+<section>
+	<ul class="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
+		{#each forms as { href, title }}
+			<li class="card p-5 bg-primary-100 flex items-center shadow-sm gap-8">
+				<a class="anchor" {href} download>{title}</a>
+			</li>
+		{/each}
+	</ul>
+</section>
--- src/routes/(main)/manuals/+page.svelte
+++ src/routes/(main)/manuals/+page.svelte
@@ -1,1 +1,37 @@
+<script lang="ts">
+	import sailingManual from '$lib/assets/documents/CSSD Sailing Manual Online 20230707 updates.pdf';
+	import dockManual from '$lib/assets/documents/CSSD Dock Leader Manual 20230711updts.pdf';
+	import martinManual from '$lib/assets/documents/Martin 16 Setup 2_02.pdf';
+	import visuallyImpairedManual from '$lib/assets/documents/Sailing Manual Online visual impaired v1.pdf';
+
+	const manuals = [
+		{
+			href: sailingManual,
+			title: 'Sailing Manual'
+		},
+		{
+			href: dockManual,
+			title: 'Dock Leader Manual'
+		},
+		{
+			href: martinManual,
+			title: 'Martin 16 Setup'
+		},
+		{
+			href: visuallyImpairedManual,
+			title: 'Sailing Manual for Visually Impaired'
+		}
+	];
+</script>
+
 <h1 class="h1">Manuals</h1>
+<hr />
+<section>
+	<ul class="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
+		{#each manuals as { href, title }}
+			<li class="card p-5 bg-primary-100 flex items-center shadow-sm gap-8">
+				<a class="anchor" {href} download>{title}</a>
+			</li>
+		{/each}
+	</ul>
+</section>
