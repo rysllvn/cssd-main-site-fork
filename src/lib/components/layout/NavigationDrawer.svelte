f5f755c575ed82a6c3ef04edc42473c0adf765dc
Ryan Sullivan <mrsulliv@gmail.com>
2023-09-05 10:35:58 -0700

    update nav drawer styling to fix home link

--- src/lib/components/layout/NavigationDrawer.svelte
+++ src/lib/components/layout/NavigationDrawer.svelte
@@ -2,6 +2,16 @@
 	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
 	import { navigation, resources } from '$lib/navigation';
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+
+	$: classesActive = (href: string) => {
+		console.log(href);
+		console.log($page.url.pathname);
+		return `/${href}` === $page.url.pathname
+			? '!bg-primary-400 !text-primary-900'
+			: '!bg-surface-100 !text-primary-900';
+	};
+
 	function drawerClose(): void {
 		drawerStore.close();
 	}
@@ -11,14 +21,14 @@
 	<nav class="list-nav p-1">
 		<ul class="p-2">
 			<li>
-				<a href="{base}/">
+				<a href="{base}/" on:click={drawerClose} class={classesActive('')}>
 					<iconify-icon icon="iconamoon:home-bold" class="mr-4 text-xl" />
 					Home
 				</a>
 			</li>
 			{#each navigation as { href, text, icon }}
 				<li>
-					<a href="{base}/{href}" on:click={drawerClose}>
+					<a href="{base}/{href}" on:click={drawerClose} class={classesActive(href)}>
 						<iconify-icon {icon} class="mr-4 text-xl" />
 						{text}
 					</a>
@@ -29,7 +39,7 @@
 		<ul class="p-2">
 			{#each resources as { href, text, icon }}
 				<li>
-					<a href="{base}/{href}" on:click={drawerClose}>
+					<a href="{base}/{href}" on:click={drawerClose} class={classesActive(href)}>
 						<iconify-icon {icon} class="mr-4 text-xl" />
 						{text}
 					</a>
